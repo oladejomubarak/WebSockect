99ed617160dac4d4c2f752c7fc8d186217d8da02
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-06 11:26:22 +0100

    setting up files

---
+++ src/main/java/com/example/pushnotificationwithwebsocket/PushNotificationWithWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.pushnotificationwithwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationWithWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushNotificationWithWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pushnotificationwithwebsocket/PushNotificationWithWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pushnotificationwithwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationWithWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
