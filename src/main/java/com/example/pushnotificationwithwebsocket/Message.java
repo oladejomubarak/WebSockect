349ebba793d371758cedaf83d66ab5abb92c8cd7
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-08 09:26:18 +0100

    addig message broker registry

---
+++ src/main/java/com/example/pushnotificationwithwebsocket/Message.java
@@ -0,0 +1,22 @@
+package com.example.pushnotificationwithwebsocket;
+
+public class Message {
+    private String text;
+    private String to;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+}
--- src/main/java/com/example/pushnotificationwithwebsocket/SecurityConfig.java
+++ src/main/java/com/example/pushnotificationwithwebsocket/SecurityConfig.java
@@ -2,15 +2,21 @@ package com.example.pushnotificationwithwebsocket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable().authorizeHttpRequests()
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/ws/**")
+                .permitAll().anyRequest().authenticated()).formLogin(withDefaults());
+        return http.build();
     }
 }
